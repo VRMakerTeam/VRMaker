faa721eaf323e81c62ed6804eda07524bdba8581
Charles Cyril Nettey <cyrilselasi@ymail.com>
2018-05-04 12:09:14 +0800

    Add quotes to paths
    Prevent invalid paths when system paths have spaces.

--- 3.run.bat
+++ 3.run.bat
@@ -1,7 +1,9 @@
-xcopy /E/Y %VRMAKER_ROOT%\creator-library\res runpath\res\
-%QT_ROOT%\bin\rcc --binary res\app.qrc -o runpath\res\app.rcc
-%QT_ROOT%\bin\rcc --binary res\fonts.qrc -o runpath\res\fonts.rcc
+xcopy /E/Y "%VRMAKER_ROOT%\creator-library\res" runpath\res\
+"%QT_ROOT%\bin\rcc" --binary res\app.qrc -o runpath\res\app.rcc
+"%QT_ROOT%\bin\rcc" --binary res\fonts.qrc -o runpath\res\fonts.rcc
 xcopy /E/Y runtime\* runpath\
 
 cd runpath
 ..\creator-app\bin\app.exe
+
+cmd /k
